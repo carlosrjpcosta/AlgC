9d9bcb329af68044bbe8a7fdc8bf9da1fd6d8d8c
Carlos Costa <51168859+carlosrjpcosta@users.noreply.github.com>
2019-05-30 11:57:41 +0100

    Add files via upload

---
+++ aula7/cmake-build-debug/CMakeFiles/aula7.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aula7.dir/main.c.o"
+  "aula7.pdb"
+  "aula7.exe"
+  "libaula7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/aula7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ aula8/cmake-build-debug/CMakeFiles/aula8.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aula8.dir/main.c.o"
+  "aula8.pdb"
+  "aula8.exe"
+  "libaula8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/aula8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
