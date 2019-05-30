94b5ae4093fa3f679c5ecafe312f4d773bf17e1f
Carlos Costa <carlospalmacosta@gmail.com>
2019-05-30 14:59:23 +0100

    Add files via upload

---
+++ aula9/cmake-build-debug/CMakeFiles/aula9.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/abp.c" "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/cmake-build-debug/CMakeFiles/aula9.dir/abp.c.o"
+  "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/fraction.c" "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/cmake-build-debug/CMakeFiles/aula9.dir/fraction.c.o"
+  "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/queue.c" "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/cmake-build-debug/CMakeFiles/aula9.dir/queue.c.o"
+  "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/simabp.c" "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/cmake-build-debug/CMakeFiles/aula9.dir/simabp.c.o"
+  "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/stack.c" "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/cmake-build-debug/CMakeFiles/aula9.dir/stack.c.o"
+  "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/testabp.c" "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/cmake-build-debug/CMakeFiles/aula9.dir/testabp.c.o"
+  "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/testfraction.c" "/cygdrive/c/Users/carlo/OneDrive/Ambiente de Trabalho/AlC/aula9/cmake-build-debug/CMakeFiles/aula9.dir/testfraction.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ aula9/cmake-build-debug/CMakeFiles/aula9.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aula9.dir/fraction.c.o"
+  "CMakeFiles/aula9.dir/testfraction.c.o"
+  "CMakeFiles/aula9.dir/queue.c.o"
+  "CMakeFiles/aula9.dir/stack.c.o"
+  "CMakeFiles/aula9.dir/abp.c.o"
+  "CMakeFiles/aula9.dir/testabp.c.o"
+  "CMakeFiles/aula9.dir/simabp.c.o"
+  "aula9.pdb"
+  "aula9.exe"
+  "libaula9.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/aula9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
