e62e04714b9bbe32ebf6f34753ea027594118096
Carlos Costa <51168859+carlosrjpcosta@users.noreply.github.com>
2019-05-30 11:54:19 +0100

    Add files via upload

---
+++ aula4_2/cmake-build-debug/CMakeFiles/aula4_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aula4_2.dir/main.c.obj"
+  "aula4_2.pdb"
+  "aula4_2.exe"
+  "aula4_2.exe.manifest"
+  "libaula4_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/aula4_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
